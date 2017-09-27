01690a0e953fac497a6a8581f5ff644cd292c3b5
Brandon Cone <bcone@esu10.org>
2017-09-27 13:43:00 -0500

    Remove print_r from index.php and add test.php for test script usage

--- container-files/var/www/html/index.php
+++ container-files/var/www/html/index.php
@@ -1,5 +1,5 @@
 <h1>This is the index.php file</h1>
 <?php
-print_r($_SERVER);
+// print_r($_SERVER);
 phpinfo();
 
---
+++ container-files/var/www/html/test.php
@@ -0,0 +1,3 @@
+<?php
+// This is a dummy file that exists only to verify the functionality of PHP through Apache.
+die(TRUE);
