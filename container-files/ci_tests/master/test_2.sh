fece67bd0c8abdc43fae17c957f57611991c079d
Brandon Cone <bcone@esu10.org>
2017-09-27 11:11:58 -0500

    adding some basic CI test scripts

---
+++ container-files/ci_tests/master/test_2.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+#This is a placeholder file that should be deleted when actual tests are run and before deploying.
+
+echo "This is test 2 and we shouldn't get here. If we do then something didn't work."
---
+++ container-files/ci_tests/tags/test_2.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+#This is a placeholder file that should be deleted when actual tests are run and before deploying.
+
+echo "Test 2";
