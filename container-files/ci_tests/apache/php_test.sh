2eeb699701da677b83736f2ce387ce45863da9f8
Brandon Cone <bcone@esu10.org>
2017-09-28 11:41:43 -0500

    move test scripts into new directory structure

--- container-files/ci_tests/apache/php_test.sh
+++ container-files/ci_tests/apache/php_test.sh
@@ -3,15 +3,15 @@
 PHP_DESTINATION=${PHP_DESTINATION:=localhost/test.php}
 
 echo "Executing curl command against ${PHP_DESTINATION}."
-RET_VAL=$( curl ${PHP_DESTINATION} )
+RET_VAL=$( curl -s ${PHP_DESTINATION} )
 # Debugging line, if having issues with this test, try inspecting the returned value/.
 # echo "Ret val is ${RET_VAL}";
 
 # Because the apache test runs before this file, if we got here then we can
 # safely assume that apache is working so test that our test.php file is returning TRUE and not text.
 if [[ $RET_VAL != 1 ]]; then
-    echo "test.php returned non-true value (assume text) indicating non-interpreted php - TEST FAILED."
+    echo "${PHP_DESTINATION} returned non-true value (assume text) indicating non-interpreted php - TEST FAILED."
     exit 1;
 else
-    echo "Test.php returned interpreted value of TRUE indicating php and apache are working - TEST PASSED."
+    echo "${PHP_DESTINATION} returned interpreted value of TRUE indicating php and apache are working - TEST PASSED."
 fi   
--- container-files/ci_tests/tags/test_2.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-#This is a placeholder file that should be deleted when actual tests are run and before deploying.
-
-echo "Test 2";
